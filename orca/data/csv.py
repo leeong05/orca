7e92fd2355a96bdf2f265bc4c70ec4f187b3625e
leeong05 <leeong05@qq.com>
2014-11-06 10:41:02 +0800

    add csv io tools

--- orca/data/csv.py
+++ orca/data/csv.py
@@ -14,12 +14,30 @@ from orca.data import (
 class CSVSaver(SaverBase):
     """Class for saving data in csv format files."""
 
-    def save(self, name, data, **kwargs):
-        data.to_csv(os.path.join(self.cachedir, name), **kwargs)
+    def _save(self, name, data, **kwargs):
+        fname = os.path.join(self.cachedir, name)
+        if os.path.exists(fname):
+            self.warning('Existing file will be overwritten: {0!r}'.format(fname))
+        params = self.params.copy()
+        params.update(kwargs)
+        with open(fname, 'w') as file:
+            data.to_csv(file, **params)
+        self.debug('Saved data in file: {0!r}'.format(fname))
+        self.datafiles[name] = fname
 
 
 class CSVLoader(LoaderBase):
     """Class for saving data in csv format files."""
 
-    def load(self, name, **kwargs):
-        pd.read_csv(os.path.join(self.cachedir, name), **kwargs)
+    def __init__(self, cachedir, **kwargs):
+        LoaderBase.__init__(self, cachedir, **kwargs)
+        self.params = {
+                'header': 0,
+                'parse_dates': False,
+                'index_col': 0,
+                }
+
+    def _load(self, name, **kwargs):
+        params = self.params.copy()
+        params.update(kwargs)
+        return pd.read_csv(os.path.join(self.cachedir, name), **params)
