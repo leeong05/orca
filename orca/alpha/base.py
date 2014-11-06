800818f38d5d5f5e2b82a4959a219a979d4f240b
leeong05 <leeong05@qq.com>
2014-11-06 20:25:00 +0800

    modify get_alphas method

--- orca/alpha/base.py
+++ orca/alpha/base.py
@@ -34,32 +34,39 @@ class AlphaBase(object):
 
         :param str date: a 8-length date string like ``yyyymmdd``.
         """
+
         raise NotImplementedError
 
     def set_debug_mode(self, debug_on):
         """Enable/Disable debug level messages in the alpha.
         This is enabled by default."""
+
         level = logging.DEBUG if debug_on else logging.INFO
         self.logger.setLevel(level)
 
     def debug(self, msg):
         """Logs a message with level DEBUG on the alpha logger."""
+
         self.logger.debug(msg)
 
     def info(self, msg):
         """Logs a message with level INFO on the alpha logger."""
+
         self.logger.info(msg)
 
     def warning(self, msg):
         """Logs a message with level WARNING on the alpha logger."""
+
         self.logger.warning(msg)
 
     def error(self, msg):
         """Logs a message with level ERROR on the alpha logger."""
+
         self.logger.error(msg)
 
     def critical(self, msg):
         """Logs a message with level CRITICAL on the alpha logger."""
+
         self.logger.critical(msg)
 
 
@@ -74,15 +81,17 @@ class BacktestingAlpha(AlphaBase):
     def __init__(self, *args, **kwargs):
         AlphaBase.__init__(self, **kwargs)
         self.alphas = {}
+        self._alphas = None
 
-    def get_alphas(self, datetime_index=True):
-        """Return the generated alphas in a DataFrame.
+    def get_alphas(self):
+        """Return the generated alphas in a DataFrame."""
 
-        :param boolean datetime_index: Whether to format the DataFrame with DatetimeIndex. Default: True
-        """
+        if self._alphas is not None:
+            return self._alphas
+
         df = pd.DataFrame(self.alphas).T
-        if datetime_index:
-            df.index = pd.to_datetime(df.index)
+        df.index = pd.to_datetime(df.index)
+        self._alphas = df
         return df
 
 
