310199762c34990ae2131d1a15e2e7d0103d7ccf
leeong05 <leeong05@qq.com>
2014-11-06 10:40:48 +0800

    modify tests for data.base

--- tests/data/base_test.py
+++ tests/data/base_test.py
@@ -17,22 +17,28 @@ from orca.data import (
 
 class IOBaseDummy(IOBase):
 
-    def save(self, name, data, **kwargs):
+    def _save(self, name, data, **kwargs):
         raise NotImplementedError
 
-    def load(self, name, **kwargs):
+    def _load(self, name, **kwargs):
+        raise NotImplementedError
+
+    def _delete(self, fnmae):
         raise NotImplementedError
 
 
 class SaverBaseDummy(SaverBase):
 
-    def save(self, name, data, **kwargs):
+    def _save(self, name, data, **kwargs):
+        raise NotImplementedError
+
+    def _delete(self, fnmae):
         raise NotImplementedError
 
 
 class LoaderBaseDummy(LoaderBase):
 
-    def load(self, name, **kwargs):
+    def _load(self, name, **kwargs):
         raise NotImplementedError
 
 
@@ -41,12 +47,13 @@ class DataTestCase(unittest.TestCase):
     def setUp(self):
         self.io = IOBaseDummy('')
         self.dir = tempfile.mkdtemp()
-        self.saver = SaverBaseDummy(os.path.join(self.dir, 'cache'))
+        self.saverplain = SaverBaseDummy(os.path.join(self.dir, 'cache'))
+        self.savernon = SaverBaseDummy(os.path.join(self.dir, 'noncache'), plain=False)
         self.loader = LoaderBaseDummy(self.dir)
 
     def tearDown(self):
         self.io = None
-        self.saver = None
+        self.saverplain = None
         self.loader = None
         shutil.rmtree(self.dir)
 
@@ -66,13 +73,16 @@ class DataTestCase(unittest.TestCase):
         self.assertEqual(self.io.logger.level, logging.DEBUG)
 
     def test_saver_load_exception(self):
-        self.assertRaises(NotImplementedError, self.saver.load, '')
+        self.assertRaises(NotImplementedError, self.saverplain._load, '')
 
     def test_loader_save_exception(self):
-        self.assertRaises(NotImplementedError, self.loader.save, '', '')
+        self.assertRaises(NotImplementedError, self.loader._save, '', '')
 
-    def test_saver_cachedir_created(self):
-        self.assertTrue(os.path.exists(self.saver.cachedir))
+    def test_plain_saver_cachedir_created(self):
+        self.assertTrue(os.path.exists(self.saverplain.cachedir))
+
+    def test_nonplain_saver_cachedir_not_created(self):
+        self.assertFalse(os.path.exists(self.savernon.cachedir))
 
     def test_loader_cachedir_exception(self):
         self.assertRaises(IOError, LoaderBaseDummy, os.path.join(self.dir, 'cached'))
