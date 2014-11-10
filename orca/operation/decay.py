efb5d550920dba5e02eb7154e514a10a10a85416
leeong05 <leeong05@qq.com>
2014-11-10 17:00:08 +0800

    decay operation

---
+++ orca/operation/decay.py
@@ -0,0 +1,27 @@
+"""
+.. moduleauthor:: Li, Wang <wangziqi@foreseefund.com>
+"""
+
+import numpy as np
+
+from base import OperationBase
+
+
+class DecayOperation(OperationBase):
+    """Class to linearly combine current alpha with decayed alphas, usually to reduce turnover.
+
+    :param int days: How many days of decayed alphas to be included in final result
+    :param boolean dense: Whether to treat ``NaN`` as 0 in current alpha. Default: False
+    """
+
+    def __init__(self, days, dense=False, **kwargs):
+        OperationBase.__init__(self, **kwargs)
+        self.days = days
+        self.dense = dense
+
+    def operate(self, alpha):
+        current = (alpha.fillna(0) if self.dense else alpha) * self.days
+        for i in range(1, self.days):
+            current += alpha.shift(i).fillna(0) * (self.days - i)
+        current[alpha.fillna(method='ffill', limit=self.days-1).isnull()] = np.nan
+        return current
