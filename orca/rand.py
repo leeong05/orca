b51148400973d8087e48718e0ca7aa7a0a7f4e64
leeong05 <leeong05@qq.com>
2014-11-10 16:56:58 +0800

    useful in generating alphas for test

---
+++ orca/rand.py
@@ -0,0 +1,18 @@
+"""
+.. moduleauthor:: Li, Wang <wangziqi@foreseefund.com>
+"""
+
+import numpy as np
+import pandas as pd
+
+from orca import (
+        DATES,
+        SIDS,
+        )
+
+def random_alpha(n=None):
+    if not n >= 0:
+        n = np.random.randint(50, 100)
+    dates = pd.to_datetime(DATES[2000:2000+n])
+    df = pd.DataFrame(np.random.randn(n, len(SIDS)), index=dates, columns=SIDS)
+    return df
