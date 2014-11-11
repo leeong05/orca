8c55aad607b07abce2546306e1831e80cf88feb3
leeong05 <leeong05@qq.com>
2014-11-11 20:56:21 +0800

    redesigned index_quote and its fetcher

--- orca/mongo/index.py
+++ orca/mongo/index.py
@@ -2,6 +2,8 @@
 .. moduleauthor:: Li, Wang <wangziqi@foreseefund.com>
 """
 
+import pandas as pd
+
 from orca import DB
 
 from base import KDayFetcher
@@ -11,33 +13,9 @@ class IndexQuoteFetcher(KDayFetcher):
     """Class to fetch index quote data."""
 
     index_dname = {
-            'SHZS': 'SH000001',
-            'AGZS': 'SH000002',
-            'BGZS': 'SH000003',
-            'ZHZS': 'SH000008',
-            'SH380': 'SH000009',
-            'SH180': 'SH000010',
-            'SH150': 'SH000133',
-            'SH100': 'SH000132',
-            'SH50': 'SH000016',
-            'JJZS': 'SH000011',
-            'GZZS': 'SH000012',
-            'SHXZZ': 'SH000017',
-            'HS300': 'SH0003000',
-            'SZCZ': 'SZ399001',
-            'CFAZ': 'SZ399002',
-            'CFBZ': 'SZ399003',
-            'SZ100': 'SZ399004',
-            'ZXBI': 'SZ399005',
-            'CYBI': 'SZ399006',
-            'SHXZS': 'SZ399100',
-            'ZXBZ': 'SZ399101',
-            'SZZZ': 'SZ399106',
-            'SZAZ': 'SZ399107',
-            'SZBZ': 'SZ399108',
-            'ZXBR': 'SZ399333',
-            'CYBR': 'SZ399606',
-            'JCAZ': 'SZ399317',
+            'HS300': 'SH000300',
+            'CS500': 'SH000905',
+            'CS800': 'SH000906',
             }
 
     def __init__(self, **kwargs):
@@ -47,7 +25,22 @@ class IndexQuoteFetcher(KDayFetcher):
             self.warning('Force reindex to be False')
             self.reindex = False
 
-    def fetch_window(self, *args, **kwargs):
-        if kwargs.get('reindex', False) is True:
-            kwargs['reindex'] = False
-        super(IndexQuoteFetcher, self).fetch_window(*args, **kwargs)
+    def fetch_window(self, dname, window, **kwargs):
+        """
+        :param dname: Data name or a list of data names
+        :type dname: str, list
+        :returns: Series if ``dname`` is only a string or DataFrame with ``dname`` in the columns
+
+        """
+        index = kwargs.pop('index')
+        datetime_index = kwargs.get('datetime_index', self.datetime_index)
+
+        query = {'index': index, 'date': {'$gte': window[0], '$lte': window[-1]}}
+        proj = {'_id': 0, 'index': 0, 'date': 1}
+        _dname = [dname] if isinstance(dname, str) else dname
+        for d in _dname:
+            proj.update({d: 1})
+        cursor = self.collection.find(query, proj)
+        df = pd.DataFrame(list(cursor))
+        df.index = pd.to_datetime(df.date) if datetime_index else df.date
+        return df[dname]
