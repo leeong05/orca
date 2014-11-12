18fcf9fdaf2c2494218c8c8d517dac78c4967f6d
leeong05 <leeong05@qq.com>
2014-11-12 18:00:04 +0800

    add methods into apis

--- orca/operation/api.py
+++ orca/operation/api.py
@@ -7,8 +7,6 @@ import pandas as pd
 
 from orca import SIDS
 
-from decay import DecayOperation
-
 def format(df, value=np.nan):
     df = df.reindex(columns=SIDS, copy=True).fillna(value)
     df.index = pd.to_datetime(df.index)
@@ -46,5 +44,36 @@ def quantiles(df, n):
 Helper APIs from operation classes
 """
 
+from decay import DecayOperation
+from barra import (
+        BarraFactorNeutOperation,
+        BarraFactorCorrNeutOperation,
+        )
+from neutralize import (
+        GroupNeutOperation,
+        IndustryNeutOperation,
+        )
+
 def decay(df, n, dense=False):
     return DecayOperation(n, dense=dense).operate(df)
+
+def barra_neut(df, model, factors):
+    return BarraFactorNeutOperation(model).operate(df, factors)
+
+def barra_corr_neut(df, model, factors):
+    return BarraFactorCorrNeutOperation(model).operate(df, factors)
+
+def group_neut(df, group=None):
+    return GroupNeutOperation(group).operate(df)
+
+def industry_neut(df, group, standard='SW2014'):
+    return IndustryNeutOperation(standard).operate(df, group)
+
+def level1_neut(df, standard='SW2014'):
+    return industry_neut(df, 'level1', standard=standard)
+
+def level2_neut(df, standard='SW2014'):
+    return industry_neut(df, 'level2', standard=standard)
+
+def level3_neut(df, standard='SW2014'):
+    return industry_neut(df, 'level3', standard=standard)
