20b7bd40823eac4c3d5a17d5d2d610c4a4a9ef7b
leeong05 <leeong05@qq.com>
2014-11-21 19:23:14 +0800

    recommended python modules

---
+++ pkgs.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/sh
+
+pip install -U setuptools
+pip install -U requests
+pip install -U nose
+
+pip install -U Cython
+pip install -U numpy
+pip install -U scipy
+pip install -U scikit-learn
+
+pip install -U matplotlib
+pip install -U pytz
+pip install -U python-dateutil
+pip install -U numexpr
+pip install -U Bottle
+pip install -U lxml
+pip install -U ipython[all]
+pip install -U pandas
+pip install -U patsy
+pip install -U statsmodels
+
+pip install -U blockdiag
+pip install -U Sphinx
+pip install -U sphinxcontrib-blockdiag
+
+pip install -U pymssql
+easy_install -U mysql-connector-python
+
+easy_install -U TA-Lib
+pip install -U tables
