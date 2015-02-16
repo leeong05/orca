142d6aa2991b5d42f35fbf294b778d3489df9136
leeong05 <leeong05@qq.com>
2015-02-16 12:43:07 +0800

    add taste to combiner config

--- examples/sample_combiner_config.py
+++ examples/sample_combiner_config.py
@@ -1,9 +1,35 @@
+import pandas as pd
+
 from orca.combiner.regression import (
         OLSCombiner,
         QuantRegCombiner,
         RidgeCombiner,
         LassoCombiner,
         )
+
+class MyOLSCombiner(OLSCombiner):
+
+    def normalize(self):
+        self.orginal_data = self.data.copy()
+        Ys = []
+        for date, df in self.data.groupby('date'):
+            Y = df.returns.copy()
+            pos = Y >= Y.quantile(0.75)
+            Y[pos] = 1
+            Y[~pos] = -1
+            Ys.append(Y)
+        self.data['returns'] = pd.concat(Ys)
+        Y = self.data.returns.copy()
+        pos = self.data[self.data.returns == 1]
+        neg = self.data[self.data.returns == -1]
+        self.data = pd.concat([pos, pos, pos, neg])
+
+    def fit(self, X, Y):
+        results = sm.OLS(Y, X).fit()
+        self.info('Regression summary:\n{}'.format(results.summary()))
+        X = self.original_data.iloc[:, 2:-1]
+        return pd.Series(results.predict(X), index=X.index).unstack()
+
 
 groups = {
         'wdfund': {
