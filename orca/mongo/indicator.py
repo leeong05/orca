cbaa25b6f531896b95b42be10952a5e4fc9c0a0a
leeong05 <leeong05@qq.com>
2015-06-29 12:40:45 +0800

    add indicator fetcher

---
+++ orca/mongo/indicator.py
@@ -0,0 +1,40 @@
+"""
+.. moduleauthor:: Li, Wang <wangziqi@foreseefund.com>
+"""
+
+import pandas as pd
+
+from orca import DB
+
+from base import KDayFetcher
+
+
+class IndicatorFetcher(KDayFetcher):
+    """Base class to fetch time-series indicator data.
+
+    .. note::
+
+       This is a base class and should not be used directly.
+    """
+
+    def __init__(self, **kwargs):
+        super(IndicatorFetcher, self).__init__(**kwargs)
+
+    def fetch_window(self, dname, window, univ=None, **kwargs):
+        assert univ
+        datetime_index = kwargs.get('datetime_index', self.datetime_index)
+        query = {'dname': dname, 'univ': univ, 'date': {'$gte': window[0], '$lte': window[-1]}}
+        proj = {'_id': 0, 'dvalue': 1, 'date': 1}
+        cursor = self.collection.find(query, proj)
+        ser = pd.Series({row['date']: row['dvalue'] for row in cursor})
+        del cursor
+        if datetime_index:
+            ser.index = pd.to_datetime(ser.index)
+        return ser
+
+
+class BarraIndicatorFetcher(IndicatorFetcher):
+
+    def __init__(self, **kwargs):
+        super(BarraIndicatorFetcher, self).__init__(**kwargs)
+        self.collection = DB.barra_indicator
